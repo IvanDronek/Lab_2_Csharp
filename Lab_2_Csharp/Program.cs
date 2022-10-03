e141e1714bb5688f8eb9eb30fb02ba288f3cf961
IvanDronek <99011404+IvanDronek@users.noreply.github.com>
2022-10-03 09:51:18 +0300

    Update Program.cs

--- Lab_2_Csharp/Program.cs
+++ Lab_2_Csharp/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,74 @@ namespace Lab_2_Csharp
     {
         static void Main(string[] args)
         {
+            //Task1
+
+            /*int a = Convert.ToInt32((Console.ReadLine()));
+            int b = Convert.ToInt32((Console.ReadLine()));
+
+            int count = 0;
+            int sum = 0;
+            for (int x = a; x < b; x++)
+                if (x % 3 == 0)
+                {
+                    count = count + 1;
+                }
+            Console.WriteLine(count);
+            Console.ReadKey();*/
+
+            //Task2
+
+            /*string str = "dasfasjpfgasjpgraeawsh";
+            for (int i = 1; i < str.Length; i += 2)
+            {
+                Console.WriteLine(str[i]);
+            }
+            Console.ReadKey();*/
+
+            //Task3
+
+            /*string drink = Console.ReadLine();
+            switch (drink)
+            {
+                
+                case "Кава":
+                case "кава":
+                    Console.WriteLine("50 грн");
+                    break;
+                
+                case "Чай":
+                case "чай":
+                    Console.WriteLine("45 грн");
+                    break;
+                
+                case "Сік":
+                case "сік":
+                    Console.WriteLine("30 грн");
+                    break;
+                
+                case "Вода":
+                case "вода":
+                    Console.WriteLine("20 грн");
+                    break;
+            }
+            Console.ReadKey();*/
+
+            //Task4
+            //Task5
+            //Task6
+
+            /*double a = double.Parse(Console.ReadLine());
+            var x1 = (a) % 10;
+            var x2 = (a) % 10;
+            int sum = (int)(x1 + x2);
+            Console.WriteLine("Сума цифр: " + sum);
+            Console.ReadKey();*/
+
+            //Task7
+            //Task8
+            //Task9
+            //Task10
+
         }
     }
 }
