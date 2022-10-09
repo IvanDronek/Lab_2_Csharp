71cf0b38789b9c7167ecbfcf938d59822c3d124f
Ivan Dronek <nomi29251@gmail.com>
2022-10-09 12:02:00 +0300

    Laboratory 2:Tasks 1-10

--- Lab_2_Csharp/Program.cs
+++ Lab_2_Csharp/Program.cs
@@ -189,6 +189,39 @@ namespace Lab_2_Csharp
                     break;
             }
             Console.ReadKey();*/
+
+            //Task10
+
+            /*int tenNum;
+            int count = 0;
+            int sum = 0;
+            int mult = 1;
+            int result = 0;
+            for (var i = 0; i < 10; i += 1)
+            {
+                tenNum = Convert.ToInt32(Console.ReadLine());
+                if (tenNum > 0)
+                {
+                    count++;
+                }
+                if (i < 5)
+                {
+                    sum += tenNum;
+                }
+                if (i < 4)
+                {
+                    mult *= tenNum;
+                }
+                if (count == 10)
+                {
+                    result = sum;
+                }
+                else if (count != 10)
+                {
+                    result = mult;
+                }
+                Console.WriteLine(result);
+            }*/
         }
     }
 }
